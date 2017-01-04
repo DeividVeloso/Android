4e0328839fbfbb6321ac72b54c95bb61b5e5a01c
Deivid Higino Veloso <dveloso@acessocard.com.br>
2017-01-04 09:56:36 -0200

    add ciclo de vida onRedume ListaActivity

--- app/src/main/java/br/com/veloso/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/veloso/agenda/ListaAlunosActivity.java
@@ -8,18 +8,19 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import java.util.List;
+
+import br.com.veloso.agenda.dao.AlunoDAO;
+import br.com.veloso.agenda.modelo.Aluno;
+
 public class ListaAlunosActivity extends AppCompatActivity {
 
     private Button btnAdd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
-
-        String[] alunos = {"Deivid", "Ana Paula", "Rafael", "Sandro", "Paulo","Lilian", "Kevin", "Bruno", "Marrone"};
-        ListView listaAlunos = (ListView)findViewById(R.id.lista_alunos);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,alunos);
-        listaAlunos.setAdapter(adapter);
 
         btnAdd = (Button) findViewById(R.id.lista_novoaluno);
 
@@ -30,5 +31,23 @@ public class ListaAlunosActivity extends AppCompatActivity {
                 startActivity(intentVaiProFormulario);
             }
         });
+    }
+
+    private void carregaLista() {
+        //Buscando alunos do banco de dados
+        AlunoDAO dao = new AlunoDAO(this);
+        List<Aluno> alunos = dao.buscaAlunos();
+        dao.close();
+
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+        //ArrayAdpter de Alunos
+        ArrayAdapter<Aluno> adapter = new ArrayAdapter<Aluno>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        carregaLista();
     }
 }
--- app/src/main/java/br/com/veloso/agenda/dao/AlunoDAO.java
+++ app/src/main/java/br/com/veloso/agenda/dao/AlunoDAO.java
@@ -2,8 +2,12 @@ package br.com.veloso.agenda.dao;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.veloso.agenda.modelo.Aluno;
 
@@ -41,5 +45,38 @@ public class AlunoDAO extends SQLiteOpenHelper {
 
         //Qual é a tabela, linha em branco, Content Values(colunas do da tabela, dictionay chave e valor)
         db.insert("Alunos", null, dados);
+    }
+
+    public List<Aluno> buscaAlunos() {
+        String sql = "Select * from Alunos;";
+        SQLiteDatabase db = getReadableDatabase();
+        //execSQL não retorna resultado, somente executa a query
+        //db.execSQL(sql);
+        //RawQuery devolve um resultado
+        //Query SQL, Parametros da query para colocar no where
+        //Ele retorna um Cursor
+        Cursor c = db.rawQuery(sql, null);
+
+        //Cursor primeiro sempre aponta para uma linha em branco, a partir da segunda é que tras os resultados
+        //Dessa forma preciso pedir para ele ir para próxima linha, ele devolve um bolean falando se acabaram as linhas ou não
+        //Percorrer com um while
+        List<Aluno> alunos = new ArrayList<Aluno>();
+        while (c.moveToNext()) {
+            Aluno aluno = new Aluno();
+            //Pega o valor da coluna do banco de dados
+            aluno.setId(c.getLong(c.getColumnIndex("id")));
+            aluno.setNome(c.getString(c.getColumnIndex("nome")));
+            aluno.setEndereco(c.getString(c.getColumnIndex("endereco")));
+            aluno.setTelefone(c.getString(c.getColumnIndex("telefone")));
+            aluno.setSite(c.getString(c.getColumnIndex("site")));
+            aluno.setNota(c.getDouble(c.getColumnIndex("nota")));
+
+            alunos.add(aluno);
+        }
+        //Fecha o cursor par aliberar memória do celular
+        c.close();
+
+
+        return alunos;
     }
 }
